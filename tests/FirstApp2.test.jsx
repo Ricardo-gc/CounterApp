9671b4cf07b34130c4f7f3626c71b273354c99f3
Ricardo García <ricardo.garcia@fstack.com.mx>
2023-01-19 22:29:50 -0600

    screen testing library

--- tests/FirstApp2.test.jsx
+++ tests/FirstApp2.test.jsx
@@ -1,36 +1,28 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { FirstApp } from "../src/FirstApp";
 
 describe('Pruebas es FirstApp', () => {
-    // test('debe hacer match con el snaptshot', () => {
-    //     const title = 'Hola Mundo';
-    //     const {container} = render( <FirstApp title={title} /> )
-    //     //Se asegura de que el componente no debe de cambiar, debe de regresar las mismas etiquetas y textos
-    //     expect( container ).toMatchSnapshot();
-    // });
+    const title = 'Hola Mundo';
+    const subTitle = 'Ich bin eine Subtitle';
+    test('Shouel match with snapshot', () => {
+        const {container} = render (<FirstApp title={title}/>);
+        expect(container).toMatchSnapshot();
+    });
+
+    test('Should show the message: Hola Mundo', () => {
+        render (<FirstApp title={title}/>);
+        expect(screen.getByText(title)).toBeTruthy();
+        //screen.debug();
+    });
     
-    test('FirstApp shoul return a title in h1 tag', () => {
-        const title = 'Hola Mundo';
-        const {container, getByText, getByTestId} = render( <FirstApp title={title} /> )
-        expect(getByText(title)).toBeTruthy();
-
-        //Como no se debe de hacer
-        // const h1 = container.querySelector('h1');
-        // expect(h1.innerHTML).toBe(title);
-        // expect(h1.innerHTML).toContain(title);
-
-        expect( getByTestId('test-title').innerHTML ).toBe(title);
+    test('Should show title in a h1 tag', () => {
+        render (<FirstApp title={title}/>);
+        expect(screen.getByRole('heading', {level: 1}).innerHTML).toContain(title);
     });
 
     test('Should show subtitle send by props', () => {
-        const title = 'Hola Mundo';
-        const subTitle = 'Ich bin eine Subtitle';
-        const {getByText, getAllByAltText} = render( <FirstApp title={title}  subTitle={subTitle}/> )
-        
-        expect(getByText(subTitle)).toBeTruthy();
-        //Cuando se espera que este más de una vez
-        // expect(getAllByAltText(subTitle).length).toBe(2);
+        render (<FirstApp title={title} subTitle={ subTitle }/>);
+        expect(screen.getByText(subTitle)).toBeTruthy();
+        //expect(screen.getAllByAltText(subTitle).length).toBe(2);
     });
-
-    
 });
